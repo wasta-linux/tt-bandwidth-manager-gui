69df0c046f522bf5c80071425ec949f73d35c284
Nate Marti <nate_marti@sil.org>
2020-10-07 21:41:32 +0100

    cleaned up commented-out code

--- trafficcop/worker.py
+++ trafficcop/worker.py
@@ -1,13 +1,12 @@
 """ Functions that run in background threads. """
 # All of these functions run inside of threads and use GLib to communicate back.
 
-import gi
+#import gi
 import subprocess
-import time
+#import time
 
-#from pathlib import Path
-from gi.repository import Gdk, GLib, Gtk
-gi.require_version("Gtk", "3.0")
+#from gi.repository import Gdk, GLib, Gtk
+#gi.require_version("Gtk", "3.0")
 from pathlib import Path
 
 from trafficcop import app
@@ -25,10 +24,10 @@ def handle_button_log_clicked():
         "--output=cat",
         "--no-pager",
         "--since=\'" + app.app.svc_start_time + "\'",
-        #"&"
     ]
     cmd_txt = " ".join(cmd)
     subprocess.run(cmd_txt, shell=True)
+    return
 
 def handle_button_config_clicked():
     cmd = [
@@ -36,21 +35,6 @@ def handle_button_config_clicked():
         "/etc/tt-config.yaml",
     ]
     subprocess.run(cmd)
-
-    # set_sensitive doesn't disable user clicking on buttons in this case.
-    #for b in app.app.buttons:
-    #    GLib.idle_add(b.set_sensitive, False)
-    # Neither subprocess.run nor os.system open the file as writeable.
-
-    #os.system(" ".join(cmd))
-    #filename = "/etc/tt-config.yaml"
-    #with open(filename, "w") as file:
-    #    data = file.read()
-    #    print(data)
-
-    #for b in app.app.buttons:
-    #    GLib.idle_add(b.set_sensitive, True)
-
     return
 
 def handle_config_changed():
@@ -58,35 +42,7 @@ def handle_config_changed():
     mod_time_epoch = config_file.stat().st_mtime
     #mod_time_obj = time.localtime(mod_time_epoch)
     #mod_time = time.strftime("%a %Y-%m-%d %H:%M:%S %Z", mod_time_obj)
+    app.app.update_service_props()
     read_time = app.app.svc_start_time
     print("Service Start Time =", read_time)
     print("Config file mod. time =", mod_time_epoch)
-    '''
-    text = ''
-    # Clear the label text if not empty.
-    GLib.idle_add(label.set_text, text)
-    if is_activated:
-        #text = 'Checking the Snap Store...'
-        #GLib.idle_add(wsmapp.app.label_button_source_online.set_text, text)
-        GLib.idle_add(label.hide)
-        GLib.idle_add(grid.attach, spinner, 2, 0, 1, 1)
-        GLib.idle_add(spinner.show)
-        GLib.idle_add(spinner.start)
-        if util.snap_store_accessible():
-            text = ''
-            wsmapp.app.updatable_online_list = util.get_snap_refresh_list()
-            wsmapp.app.updatable_online_dict = util.get_snap_refresh_dict()
-            wsmapp.app.select_online_update_rows()
-        else:
-            text = 'No connection to the Snap Store.'
-            wsmapp.app.button_source_online.set_active(False)
-        GLib.idle_add(spinner.stop)
-        GLib.idle_add(spinner.hide)
-        GLib.idle_add(label.show)
-    else:
-        text = ''
-        wsmapp.app.deselect_online_update_rows()
-
-    GLib.idle_add(label.set_text, text)
-    return
-    '''
