d40b87db1e749ffcf8885b7ed8af5db0c3c311d4
Nate Marti <nate_marti@sil.org>
2020-10-07 13:21:52 +0100

    fixed to refresh device name when service (re)started

--- TODO.md
+++ TODO.md
@@ -1,15 +1,19 @@
 ### Planned features
 - [x] Show managed interface name
-- [ ] Toggle button for enable/disable
-- [ ] Toggle button for start/stop
-- [ ] Button for restart
-- [ ] Button to view log
-- [ ] Button to open config file
+  - [x] Refresh name when service is (re)started
+- [x] Toggle button for enable/disable
+- [x] Toggle button for start/stop
+- [x] Button for restart
+- [x] Button to view log
+- [x] Button to open config file
+- [ ] Button to apply updated config
 - [ ] Button to reset config
 
 ### Packaging needs
 - [ ] Properly create setup.py
 - [ ] add gnome-terminal dependency
+- [ ] Create debian package folder.
+- [ ] Use debian/changelog to provide version number
 
 ### Feature wishlist
 - Set config in app window instead of text file.
--- trafficcop/handler.py
+++ trafficcop/handler.py
@@ -2,49 +2,96 @@
 
 import os
 import re
+import shutil
 import subprocess
-#import threading
+import threading
+import time
+from pathlib import Path
 
 from trafficcop import app
+from trafficcop import worker
 
 
 class Handler():
+    #def __init__(self):
+        #self.window = app.app.window
+
     def gtk_widget_destroy(self, *args):
         app.app.quit()
 
     def on_toggle_unit_state_state_set(self, widget, state):
-        state = str(state)
-        print("Unit State toggled to", state + ".")
+        sstate = str(state)
+        print("Unit State toggled to", sstate + ".")
+        # Apply new state to the service.
+        if state == True:
+            cmd = ["systemctl", "enable", "tt-bandwidth-manager.service"]
+        elif state == False:
+            cmd = ["systemctl", "disable", "tt-bandwidth-manager.service"]
+        subprocess.run(cmd)
+        # Ensure that toggle button matches true state.
+        app.app.update_state_toggles()
 
     def on_toggle_active_state_set(self, widget, state):
-        state = str(state)
-        print("Active State toggled to", state + ".")
+        # Apply new state to the service.
+        if state == True:
+            cmd = ["systemctl", "start", "tt-bandwidth-manager.service"]
+            subprocess.run(cmd)
+            app.app.wait_for_tt_start()
+        elif state == False:
+            cmd = ["systemctl", "stop", "tt-bandwidth-manager.service"]
+            subprocess.run(cmd)
+
+        # Ensure that toggle button matches true state.
+        app.app.update_state_toggles()
+        # Update managed device name.
+        app.app.update_device_name()
 
     def on_button_restart_clicked(self, folder_obj):
-        print("Restart button clicked.")
+        # Restart the service
+        cmd = ["systemctl", "restart", "tt-bandwidth-manager.service"]
+        subprocess.run(cmd)
+        # Check service status and update toggles.
+        app.app.update_state_toggles()
+        time.sleep(3)
+        app.app.update_device_name()
 
     def on_button_log_clicked(self, *args):
-        # Get the time when the service was last started.
-        cmd = [
-            "journalctl",
-            "--reverse",
-            "--unit=tt-bandwidth-manager.service",
-            "--since=-2days",
-            "--output=short-full",
-            ]
-        result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        stdout = result.stdout.decode()
-        pat = '^.*: Started.*$'
-        match = re.search(pat, stdout, re.MULTILINE)
-        matched_line = match.group(0)
-        start_time = " ".join(matched_line.split()[:4])
-
-        # Follow the log since this time in a terminal window.
-        cmd = "gnome-terminal --geometry=144x24+100+200 -- journalctl --unit=tt-bandwidth-manager.service --follow --output=cat --no-pager --since=\'" + start_time + "\'"
-        os.system(cmd)
+        target = worker.handle_button_log_clicked
+        t_log = threading.Thread(target=target)
+        t_log.start()
 
     def on_button_config_clicked(self, *args):
-        print("Config button clicked.")
+        target = worker.handle_button_config_clicked
+        t_config = threading.Thread(target=target)
+        t_config.start()
+
+        target = worker.handle_config_changed
+        t_restart = threading.Thread(target=target)
+        t_restart.start()
 
     def on_button_reset_clicked(self, button):
-        print("Reset button clicked.")
+        current = Path("/etc/tt-config.yaml")
+        # Make a backup of existing config, add index if backup already exists.
+        name = current.stem
+        suffix = ".yaml.bak"
+        backup = current.with_suffix(suffix)
+        if backup.exists():
+            # Add index to name.
+            i = 1
+            backup = current.with_name(name + '-' + str(i)).with_suffix(suffix)
+            print(backup)
+            while backup.exists():
+                # Keep trying new indices until an available one is found.
+                i += 1
+                backup = current.with_name(name + '-' + str(i)).with_suffix(suffix)
+        # Make backup copy.
+        shutil.copyfile(current, backup)
+        # Copy /usr/share/tt-bandwidth-manager/tt-default-config.yaml to /etc/tt-config.yaml;
+        #   overwrite existing file.
+        default = Path("/usr/share/tt-bandwidth-manager/tt-default-config.yaml")
+        print("copying", default, "to", current)
+        shutil.copyfile(default, current)
+        cmd = ["systemctl", "restart", "tt-bandwidth-manager.service"]
+        restart = subprocess.run(cmd)
+        # Ensure that toggle buttons match true state.
+        app.app.update_state_toggles()
