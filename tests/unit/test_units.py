8ee871a983ff3381dd51476aa6526bfb907c6674
Nate Marti <nate_marti@sil.org>
2020-10-12 13:13:10 +0100

    test using packages files instead of system files

--- tests/unit/test_units.py
+++ tests/unit/test_units.py
@@ -7,6 +7,7 @@ import unittest
 from pathlib import Path
 
 from trafficcop import app
+from trafficcop import config
 from trafficcop import utils
 
 # Assert*() methods here:
@@ -14,24 +15,28 @@ from trafficcop import utils
 
 class All(unittest.TestCase):
     def setUp(self):
-        pass
+        self.tests_dir = Path(__file__).parents[1]
+        self.pkg_dir = Path(__file__).parents[2]
 
     def test_check_diff(self):
-        example = '/usr/share/tt-bandwidth-manager/tt-example.yaml'
-        default = '/usr/share/traffic-cop/tt-default-config.yaml'
+        example = self.tests_dir / 'data' / 'tt-example.yaml'
+        default = self.tests_dir / 'data' / 'tt-default-config.yaml'
         diff = utils.check_diff(example, default)
         self.assertNotEqual(diff, 0)
 
     def test_ensure_config_backup(self):
         # This only tests that tests/data/tt-config.yaml == tests/data/tt-config.yaml.bak.
         #   It doesn't test the creation of a properly-named new backup file.
-        tests_dir = Path(__file__).parents[1]
-        current = tests_dir / 'data' / 'tt-config.yaml'
+        current = self.tests_dir / 'data' / 'tt-config.yaml'
         result = utils.mute(
             utils.ensure_config_backup,
             current
         )
         self.assertTrue(result)
+
+    def test_IGNORE(self):
+        # This is for testing new functions.
+        pass
 
     def test_INCOMPLETE_restart_service(self):
         pass
