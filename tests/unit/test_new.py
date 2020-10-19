b4666530a2c2298aa466ce2b70aa61634b740be1
Nate Marti <nate_marti@sil.org>
2020-10-19 17:42:26 +0100

    more imports to cover all modules

--- tests/unit/test_new.py
+++ tests/unit/test_new.py
@@ -1,7 +1,11 @@
 import unittest
 #from pathlib import Path
 
+from trafficcop import app
 from trafficcop import config
+from trafficcop import handler
+from trafficcop import utils
+from trafficcop import worker
 
 # Assert*() methods here:
 # https://docs.python.org/3/library/unittest.html?highlight=pytest#unittest.TestCase
